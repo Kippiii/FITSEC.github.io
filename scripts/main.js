847ffb34a7ec69c5b6b250c0cfd1c1222fd2a873
Nick Cottrell <nrc1mr.e3@gmail.com>
2019-08-26 16:30:24 -0400

    Added multiple pages with Dreamweaver Template

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+//console.log(location.pathname.split("/").slice(-1)[0]);
+file = location.pathname.split("/").slice(-1)[0];
+x = document.getElementsByTagName("nav")[0].getElementsByTagName("li");
+var y;
+for (var i=0; i<x.length; i++) {
+	y = x[i].innerHTML.split("\"")[1];
+    if (y === file) {
+        x[i].className = "active";
+    }
+}
+/*$(document).ready(function(e) {
+    $(li).select()
+});*/
