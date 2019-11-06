669af23e5ddeb331836467736ffdf25e88b01a24
kippiii <ianorzel@gmail.com>
2019-11-06 09:16:31 -0500

    Continually improving navbar

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,21 @@ for (var i = 0; i < x.length; i++) {
         x[i].className = "active";
     }
 }
+
+$(window).on('load resize', function() {
+    if(this.matchMedia('(min-width: 768px)').matches) {
+        $('.dropdown').hover(
+            function() {
+                $(this).addClass('show')
+                $(this).find($('.dropdown-toggle')).attr('aria-expanded', 'true')
+                $(this).find($('.dropdown-menu')).addClass('show')
+            }, function() {
+                $(this).removeClass('show')
+                $(this).find($('.dropdown-toggle')).attr('aria-expanded', 'false')
+                $(this).find($('.dropdown-menu')).removeClass('show')
+            }
+        )
+    }else{
+        $('.dropdown').off('mouseenter mouseleave')
+    }
+})
